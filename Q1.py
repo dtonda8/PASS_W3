4f341f1e97f9f935d54aef0927b705099420225d
David Batonda <99521514+dtonda8@users.noreply.github.com>
2024-03-10 18:06:31 +1100

    Add data structures

---
+++ Q1.py
@@ -0,0 +1,12 @@
+## import data structures using format below
+from data_structures.array_list import ArrayList
+
+MAX_LENGTH = 100
+
+def valid_parentheses(input_str: str):
+	pass
+
+if __name__ == "__main__": # for personal tests
+	# s = "()[]{}"
+	# print(valid_parentheses(s))
+	pass
---
+++ Q2.py
@@ -0,0 +1,15 @@
+## import data structures using format below
+from data_structures.array_list import ArrayList
+
+MAX_LENGTH = 200
+
+def find_duplicate_negative_number(input_list: ArrayList):
+	pass
+
+if __name__ == "__main__": # for personal tests
+	# test = ArrayList(11)
+	# for i in range(1,10):
+	# 	test.append(-1 * i)
+	# 	test.append(-4)
+	# print(find_duplicate_negative_number(test))
+	pass
