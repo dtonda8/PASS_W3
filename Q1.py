502d0b04261ab305aa34bb55eda464208161ae57
David Batonda <99521514+dtonda8@users.noreply.github.com>
2024-03-13 14:21:28 +1100

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,12 +1,21 @@
-## import data structures using format below
-from data_structures.array_list import ArrayList
+from data_structures.stack_adt import ArrayStack
 
 MAX_LENGTH = 100
 
 def valid_parentheses(input_str: str):
-	pass
+	st = ArrayStack(MAX_LENGTH)
+	for ch in input_str:
+		if ch == "(" or ch == "[" or ch == "{":
+			st.push(ch)
+		elif (st.is_empty() # Not opening bracket to match closing bracket
+			or (ch == ")" and st.peek() != "(") # mismatch
+			or (ch == "]" and st.peek() != "[") # mismatch
+			or (ch == "}" and st.peek() != "{")): # mismatch
+			return False
+		else: # matching closing bracket
+			st.pop()
+	return st.is_empty()
 
 if __name__ == "__main__": # for personal tests
-	# s = "()[]{}"
-	# print(valid_parentheses(s))
-	pass
+	s = "()[]{}"
+	print(valid_parentheses(s))
--- Q2.py
+++ Q2.py
@@ -1,15 +1,20 @@
-## import data structures using format below
 from data_structures.array_list import ArrayList
+from data_structures.bset import BSet
 
 MAX_LENGTH = 200
 
 def find_duplicate_negative_number(input_list: ArrayList):
-	pass
+	s = BSet(MAX_LENGTH)
+	for i in range(len(input_list)):
+		if -1 * input_list[i] in s:
+			return input_list[i]
+		s.add(-1 * input_list[i])
+	return -1
 
 if __name__ == "__main__": # for personal tests
-	# test = ArrayList(11)
-	# for i in range(1,10):
-	# 	test.append(-1 * i)
-	# 	test.append(-4)
-	# print(find_duplicate_negative_number(test))
+	test = ArrayList(11)
+	for i in range(1,10):
+		test.append(-1 * i)
+		test.append(-4)
+	print(find_duplicate_negative_number(test))
 	pass
