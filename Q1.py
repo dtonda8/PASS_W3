c0bf4257b3cac252e2b359d3e9742b5648e97c76
dtonda8 <davidbatonda7@gmail.com>
2024-08-10 07:06:52 +1000

    Add sols

--- Q1.py
+++ Q1.py
@@ -1,12 +1,28 @@
 ## import data structures using format below
 from data_structures.array_list import ArrayList
+from data_structures.stack_adt import ArrayStack
 
 MAX_LENGTH = 100
+OPEN_BRACKETS = "({["
+CLOSE_BRACKETS = ")}]"
 
-def valid_parentheses(input_str: str):
-	pass
+def valid_parentheses(input_str: str) -> bool:
+    # Stack for open brackets
+    open_bracket_st = ArrayStack(MAX_LENGTH)
+    for bracket in input_str:
+        if bracket in OPEN_BRACKETS:
+            open_bracket_st.push(bracket)
+            
+        elif bracket in CLOSE_BRACKETS:
+            # check if open bracket does not match closing bracket, if so, return False (invalid parentheses)
+            if (len(open_bracket_st) == 0 or # not matching bracket at all
+                OPEN_BRACKETS.index(open_bracket_st.pop()) != CLOSE_BRACKETS.index(bracket)):
+                return False
+    
+    # If no more open brackets remain, return true
+    return len(open_bracket_st) == 0
 
 if __name__ == "__main__": # for personal tests
-	# s = "()[]{}"
-	# print(valid_parentheses(s))
+	s = "()[]{}"
+	print(valid_parentheses(s))
 	pass
--- Q2.py
+++ Q2.py
@@ -1,15 +1,35 @@
 ## import data structures using format below
 from data_structures.array_list import ArrayList
+from data_structures.bset import BSet
 
 MAX_LENGTH = 200
 
-def find_duplicate_negative_number(input_list: ArrayList):
-	pass
+def find_duplicate_negative_number(input_list: ArrayList) -> int:
+	# Idea: Use set (bitset for best efficiency)
+	seen = BSet()
+	n = len(input_list)
+	for i in range(n):
+		num = input_list[i]
+		idx = get_BSet_index(num)
+		
+		# Check if seen
+		if idx in seen:
+			return num
+		
+		# Else add to set
+		seen.add(idx)
+	
+	# Return -1 by default (shouldn't be the case)
+	return -1
+
+# Function to make number appropriate for BSet
+def get_BSet_index(el: int) -> int:
+    return el * -1
 
 if __name__ == "__main__": # for personal tests
-	# test = ArrayList(11)
-	# for i in range(1,10):
-	# 	test.append(-1 * i)
-	# 	test.append(-4)
-	# print(find_duplicate_negative_number(test))
+	test = ArrayList(11)
+	for i in range(1,10):
+		test.append(-1 * i)
+	test.append(-4) # dupe
+	print(find_duplicate_negative_number(test))
 	pass
